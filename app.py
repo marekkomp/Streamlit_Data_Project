920104fcdcdb3dead246b3bef07416d606de7864
marekkomp <195009940+marekkomp@users.noreply.github.com>
2025-01-19 09:33:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# Wczytywanie danych
+# Wczytywanie danych z CSV
 try:
-    # Użyj dokładnej nazwy pliku, który wrzuciłeś do repozytorium
-    data = pd.read_excel("offers_2025-01-19(1).xlsm")
+    data = pd.read_csv("1.csv")  # Upewnij się, że nazwa pliku się zgadza
 
-    # Wyświetlanie danych w aplikacji
     st.title("Podgląd danych")
-    st.write("Poniżej znajdują się dane z pliku Excel:")
+    st.write("Poniżej znajdują się dane z pliku CSV:")
     st.write(data)
 
 except FileNotFoundError:
-    st.error("Nie znaleziono pliku Excel. Upewnij się, że plik jest w repozytorium i nazwa jest poprawna.")
+    st.error("Nie znaleziono pliku CSV. Upewnij się, że plik jest w repozytorium i nazwa jest poprawna.")
 
 except Exception as e:
     st.error(f"Wystąpił błąd podczas wczytywania pliku: {e}")
